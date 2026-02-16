718cf18bb7f23b75dae3fc074c9a8b7cb75906f9
crlarrea <crlarrea@protonmail.com>
2026-02-16 23:50:08 +0000

    finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import { FaSpotify } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      {/* <Image src={"/hero.jpg"} alt="hero" fill/> */}
+      <h1>hypnotic</h1>
+      <h2>bashful</h2>
+      <h3><FaSpotify/> now playing</h3>
       <article className={styles.hero}>
         <span>eins</span>
         <Image src={"/person.png"} alt="hero" fill />
@@ -18,6 +21,7 @@ export default function Home() {
         <span>drei</span>
         <div></div>
       </article>
+
     </main>
   );
 }
